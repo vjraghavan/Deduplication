a2b08815eb55e2278f42881e977d2df28ccc446a
Vijayaraghavan <connectingvijay@gmail.com>
2013-02-01 22:03:34 -0800

    updated write cache

---
+++ src/com/deduplication/cache/WriteCache.java
@@ -0,0 +1,26 @@
+package com.deduplication.cache;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import net.spy.memcached.MemcachedClient;
+
+public class WriteCache {
+	
+	private MemcachedClient cache;
+	
+	public WriteCache(){
+		try {
+			cache = new MemcachedClient(
+				    new InetSocketAddress("localhost", 1121));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void set(String key, Object value){
+		cache.set(key, 3600, value);
+	}
+	
+	public Object get(String key){
+		return cache.get(key);
+	}
+}
